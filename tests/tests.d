23619a524bb35d62c3c03a896145e7c27569f1d9
valmat <ufabiz@gmail.com>
2019-01-24 01:36:54 +0500

    Creating tests

--- tests/tests.d
+++ tests/tests.d
@@ -173,6 +173,9 @@ void main(string[] args)
             db.has("key:1:_1")
                 .checkTest(`db.has(existentKey)`);
 
+            db.has("key:1:_2").has
+                .checkTest(`db.has(existentKey).has`);
+
             db.has("key:1:_2").value
                 .equal("val-2(1)")
                 .checkTest(`db.has(existentKey).value`);
@@ -180,6 +183,10 @@ void main(string[] args)
             db.has("key:1:_4")
                 .not
                 .checkTest(`db.has(missingKey)`);
+
+            db.has("key:1:_5").has
+                .not
+                .checkTest(`db.has(missingKey).has`);
 
             db.has("key:1:_5").value
                 .equal("")
@@ -208,7 +215,7 @@ void main(string[] args)
 
         }
 
-        headTest("Multi delete ");
+        headTest("Multi delete");
         {
             auto range = [
                 Pair("delme_1", "ToDel*1"),
@@ -259,6 +266,83 @@ void main(string[] args)
                 .checkTest(`Check previous`);
         }
 
+        headTest("Ingrements keys");
+        {
+            db.incr("incr1")
+                .checkTest(`Check increment`);
+
+            db.incr("incr2", 5)
+                .checkTest(`Check increment`);
+
+            db.incr("incr3", -11)
+                .checkTest(`Check derement`);
+
+            db.get("incr1")
+                .equal("1")
+                .checkTest(`Check previous`);
+
+            db.get("incr2")
+                .equal("5")
+                .checkTest(`Check previous`);
+
+            db.get("incr3")
+                .equal("-11")
+                .checkTest(`Check previous`);
+        }
+
+        headTest("Array access overloading");
+        {
+            //auto values = [
+            //    "\r\n1\n%%%\t^\n",
+            //    "\r\n2\n%%%\t^\n",
+            //    "\r\n3\n%%%\t^\n",
+            //];
+            auto range = [
+                Pair("pref:_0_", "\r\n1\n%%%\t^\n"),
+                Pair("pref:_1_", "\r\n2\n%%%\t^\n"),
+                Pair("pref:_2_", "\r\n3\n%%%\t^\n"),
+            ];
+            //auto keys = range.map!(x => x.key);
+            auto keys   = range.map!(x => x.key);
+            auto values = range.map!(x => x.value);
+
+
+            db.set(range)
+                .checkTest(`set("range")`);
+
+            db[keys[0]]
+                .equal(values[0])
+                .checkTest(`db[key]`);
+
+            db[keys[0], keys[1]]
+                .equal(range[0..2])
+                .checkTest(`db[key1, key2]`);
+
+            db[keys]
+                .equal(range)
+                .checkTest(`db[keysRange]`);
+
+            keys[0..2]
+                .writeln;
+
+            db[keys[1..2]]
+                .writeln;
+                //.equal(values[0])
+                //.checkTest(`db[key]`);
+
+
+
+
+
+ 
+        }
+
+        headTest("array access overloading");
+        {
+
+        }
+
+
 
 
 
@@ -282,20 +366,7 @@ void main(string[] args)
         */
 
 
-        //`[db.del(["key-1","key-2","key-3",])]`.writeln;
-        ////[db.del(["key-1","key-2","key-3",])].writeln;
-        //[db.del("key-1","key-2","key-3")].writeln;
 
-
-
-
-
-
-
-
-
-        //writeln(`[db.get("key1", "key2", "keyq")]:`);
-        //[db.get("key1", "key2", "keyq")].writeln;
 
         //writeln(`[ db["key1"] ]:`);
         //[ db["key1"] ].writeln;
@@ -328,16 +399,6 @@ void main(string[] args)
         //`[db.get(["key-1","key-2","key-3",])]`.writeln;
         //[db.get(["key-1","key-2","key-3",])].writeln;
 
-        //`[db.incr]`.writeln;
-        //[db.incr("incr1"     )].writeln;
-        //[db.incr("incr2",   5)].writeln;
-        //[db.incr("incr3", -11)].writeln;
-        //writeln(`[db.get("incr1")]:`);
-        //[db.get("incr1")].writeln;
-        //writeln(`[db.get("incr2")]:`);
-        //[db.get("incr2")].writeln;
-        //writeln(`[db.get("incr3")]:`);
-        //[db.get("incr3")].writeln;
 
         //`[db.incr]`.writeln;
         //( db["incr4"] += 7 ).writeln;
